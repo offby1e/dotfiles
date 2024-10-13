09aa4b02831581d1faf8ad68fbce7c1282f64cb0
rkdgus0810 <84117112+rkdgus0810@users.noreply.github.com>
2024-10-13 21:32:22 +0900

    Add files via upload

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,21 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+	end,
+}
