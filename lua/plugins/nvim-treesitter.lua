68172ba24f832996a2095f2575294c6571dc1325
hexaive <hyun0810d@gmail.com>
2025-07-03 20:24:02 +0900

    backup: 2025.07.03

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,23 +1,25 @@
 return {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-          ensure_installed = { "lua", "python", "c", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-          autotag = { enable = true },
-        })
-      end
-    },
-    {
-      "windwp/nvim-ts-autotag",
-      event = "VeryLazy",
-      dependencies = "nvim-treesitter/nvim-treesitter",
-      config = true,
-    }
+      configs.setup({
+        ensure_installed = { "lua", "python", "c", "javascript", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+        -- autotag 설정은 제거합니다!
+      })
+    end
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  }
 }
